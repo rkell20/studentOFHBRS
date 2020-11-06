8d7151a56310c86b85d9abd6fba4c78d66619f28
rkelle <ruth.kellermannn>
2020-11-06 19:52:20 +0100

    Revert "My new Factory"
    This reverts commit f298f670

--- src/org/hbrs/se/ws20/uebung1/control/Factory.java
+++ src/org/hbrs/se/ws20/uebung1/control/Factory.java
@@ -1,11 +1,4 @@
 package org.hbrs.se.ws20.uebung1.control;
 
 public class Factory {
-    public Translator createTranslator(){
-        return new GermanTranslator();
-    }
-
-    public void create(){
-
-    }
 }
